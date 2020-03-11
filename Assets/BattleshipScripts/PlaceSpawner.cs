2f9f52b8bd80e563b396269f737221263a56df1a
AdharshG <34604785+Adharshg@users.noreply.github.com>
2020-03-11 19:31:02 +0530

    GitIgnore

--- Assets/BattleshipScripts/PlaceSpawner.cs
+++ Assets/BattleshipScripts/PlaceSpawner.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class PlaceSpawner : MonoBehaviour {
 
+	public GameObject BoardUnitPrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+		for (float i = -4.5f; i <= 4.5f; i++){
+			for (float j = -4.5f; j <= 4.5f; j++){
+				Debug.Log(i + " : " + j);
+				 GameObject tmp = GameObject.Instantiate(BoardUnitPrefab, new Vector3(i,j, 0), this.gameObject.transform.rotation) as GameObject;
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
